3baafc827280708cdfc22af50b7152fc721e6c9d
Jason Kearney <jkearney345@gmail.com>
2022-06-29 18:29:27 -0400

    create function for playing a round of game

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose! Paper beats rock!';
+        }
+        else if (computerSelection === 'scissors') {
+            return 'You win! Rock beats scissors!';
+        }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win! Paper beats rock!';
+        }
+        else if (computerSelection === 'scissors') {
+            return 'You lose! Scissors beats paper!';
+        }
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You lose! Rock beats scissors!';
+        }
+        else if (computerSelection === 'paper') {
+            return 'You win! Scissors beats paper!';
+        }
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
